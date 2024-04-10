59213c73a63a8f1d0c24015357a5032056e88b22
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-10 15:41:41 +0300

    create names instance

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Set<String> names = new HashSet<>();
+
+        names.add("Walter");
+        names.add("Jesse");
+        names.add("Skyler");
+        names.add("Mike");
+        names.add("Saul");
+
+        names.remove("Walter");
+//        names.remove(0);  => does not work
+
+        System.out.println(names);
+        names.clear();
+        System.out.println(names.size());
+        System.out.println(names.contains("Saul"));
+        System.out.println(names.isEmpty());
+
     }
 }
